282deb5882e93ca5c19f8abd20b28dfbaecc8c8d
sbullocks <sbullocks@gmail.com>
2022-10-30 14:27:56 -0400

    added app.listen function for the port.

--- server.js
+++ server.js
@@ -13,12 +13,16 @@ const htmlRoutes = require('./routes/htmlRoutes');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Invokes app.use() and serves static files from the '/public' folder
 app.use(express.static('public'));
 
 // Imports middleware for the routes.
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
-
+// This app starts a server and listens on port 3001 for connections. Same as server.listen().
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
 
 
